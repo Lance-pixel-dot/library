d98739c3cb839d6638f596d50c000288caf2f369
Renier Lawrence Tuico <renierlawrencetuico143@gmail.com>
2025-01-13 18:33:26 +0800

    Add form.
    Added a form that take user input and display the book that the user added.

--- script.js
+++ script.js
@@ -1,43 +1,80 @@
 const bookDisplay = document.querySelector('.card-container');
+const displayForm = document.querySelector('.form-button');
+const selForm = document.querySelector('.fill-up-form');
+const formSub = document.querySelector('.submit-form');
+const getTitle = document.querySelector('#book-title');
+const getAuthor = document.querySelector('#book-author');
+const getGenre = document.querySelector('#book-genre');
+const getPages = document.querySelector('#book-pages');
+const getDate = document.querySelector('#book-release');
+const getRadio = document.getElementsByName('book-read');
+const dialog = document.querySelector('dialog');
+const closeDialog = document.querySelector('.close-dialog');
 
 const myLibrary = [];
 
-function book(title, author, pages, genre, date){
+function book(title, author, genre, pages, date, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.genre = genre;
     this.date = date;
+    this.read = read;
 }
 
-function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookGenre, bookDate){
-    bookTitle = 'Book Name'
-    bookAuthor = 'Book Author'
-    bookPages = 'Book Pages'
-    bookGenre = 'Book Genre'
-    bookDate = 'Book Date'
+book.prototype.info = function(){
+    return `Title:${this.title} Author:${this.author} Genre:${this.genre} Pages:${this.pages} Release Date:${this.date} Have you read it yet:${this.read}`
+}
 
-    const newBook = new book(bookTitle, bookAuthor, bookPages, bookGenre, bookDate);
+function addBookToLibrary(bookTitle, bookAuthor, bookGenre, bookPages, bookDate, bookRead){
 
-    const newBook2 = new book('Book2Title', 'Book2Author', 'Book2Pages', 'Book2Genre', 'Book2Date')
+    const newBook = new book(bookTitle, bookAuthor, bookGenre, bookPages, bookDate, bookRead);
+    myLibrary.push(newBook.info());
+    displayBook();
 
-    myLibrary[0] = `${newBook.title}, ${newBook.author}, ${newBook.pages}, ${newBook.genre}, ${newBook.date}`;
-
-    myLibrary[1] = `${newBook2.title}, ${newBook2.author}, ${newBook2.pages}, ${newBook2.genre}, ${newBook2.date}`;
-    
-    console.log(myLibrary)
 }
 
 function displayBook(){
-    let createDiv;
+    let createDiv = document.createElement('div');
+    createDiv.setAttribute('class', 'cards');
+    bookDisplay.appendChild(createDiv);
 
     for(let i = 0; i < myLibrary.length; i++){
-        createDiv = document.createElement('div');
-        createDiv.setAttribute('class', 'cards');
-        bookDisplay.appendChild(createDiv);
         createDiv.textContent = myLibrary[i];
     }
 }
 
-addBookToLibrary();
-displayBook();
+selForm.addEventListener('submit', (e) => {
+    e.preventDefault();  
+})
+
+displayForm.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+closeDialog.addEventListener('click', () => {
+    dialog.close();
+})
+
+formSub.addEventListener('click', () => {
+
+    let getRadioResult = '';
+
+    getRadio.forEach(e => {
+        if (e.checked){
+            getRadioResult = e.value;
+        }
+    });
+
+    if(getTitle.value !== '' &&
+       getAuthor.value !== '' &&
+       getGenre.value !== '' &&
+       getPages.value !== '' &&
+       getDate.value !== '' &&
+       getRadioResult !== ''
+    ){
+        addBookToLibrary(getTitle.value, getAuthor.value, getGenre.value, getPages.value, getDate.value, getRadioResult);
+    }
+
+})
+
